3ad1dfdb4a7950e1fa5fc8729c66c6dfe86b5fcf
dimau <dimau777@gmail.com>
2018-06-25 17:38:22 +0500

    test virtual hosting

--- forimport.py
+++ forimport.py
@@ -1,6 +1,17 @@
 #!/home/host1423612/.virtualenvs/eventer-env/bin/python
 #  -*- coding: UTF-8 -*-
 
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World! Flaaaaaask"
+
+
+"""
 print("Content-Type: text/plain\r\n\r\n")
 print("")
 print("Hello World! Import")
+"""
