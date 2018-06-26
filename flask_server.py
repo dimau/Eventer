7378e5c306b37b9590123d41eb1d77933dc92fc0
dimau <dimau777@gmail.com>
2018-06-26 18:18:21 +0500

    test virtual hosting

--- flask_server.py
+++ flask_server.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     return """{
   "fulfillmentText": "This is a text response",
