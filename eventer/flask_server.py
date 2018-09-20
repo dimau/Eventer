8192e2eabbdb1b70e2891ef6c9b16a55fa61bbfb
«Dmitry <dimau777@gmail.com>
2018-09-20 21:35:42 +0300

    Some new logic about panel of administration

--- eventer/flask_server.py
+++ eventer/flask_server.py
@@ -1,12 +1,16 @@
 #  -*- coding: UTF-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route("/panadmin/", methods=["GET", "POST"])
 def panadmin():
-    return render_template("panadmin.html")
+    answer_string = ""
+    if request.method == 'POST':
+        source_url = request.form['source_url']
+        answer_string = "Вот здесь ответ из базы данных"
+    return render_template("panadmin.html", answer_string=answer_string)
 
 if __name__ == "__main__":
     app.run()
