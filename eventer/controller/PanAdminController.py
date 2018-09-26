a9d04cb0e66165f2e0ae1663ffca5c61405ece68
«Dmitry <dimau777@gmail.com>
2018-09-26 20:33:19 +0300

    Functionality of checking source url and getting information about an appropriate event from DB is working

--- eventer/controller/PanAdminController.py
+++ eventer/controller/PanAdminController.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import sqlalchemy
 from AbstractController import AbstractController
 from Event import Event
 from flask import render_template, request
+# it's needed for operation relationship between User, Event and Rating
+from User import User
+from Rating import Rating
 
 
 class PanAdminController(AbstractController):
@@ -13,8 +17,23 @@ class PanAdminController(AbstractController):
     def index_page(self):
         self.session = self._create_session_with_db()
         answer_string = ""
+
         if request.method == 'POST':
+
+            # We have check both urls: with "/" at the end and without "/" at the end
             source_url = request.form['source_url']  # https://kudago.com/msk/event/fleshmob-fleshmob-ot-platyozhnoj-sistemyi-mir/
-            relevant_events = self.session.query(Event).filter(Event._url == source_url).first()
-            answer_string = relevant_events.repr()
+            if source_url and source_url[-1] == "/":
+                source_url2 = source_url[0:-1]
+            else:
+                source_url2 = source_url + "/"
+
+            # Trying to get an event from the database by url
+            relevant_event = self.session.query(Event).filter(sqlalchemy.or_(Event._url == source_url, Event._url == source_url2)).first()
+
+            # Making an answer
+            if relevant_event:
+                answer_string = repr(relevant_event)
+            else:
+                answer_string = "There is not information about event with url: " + source_url
+
         return render_template("panadmin.html", answer_string=answer_string)
--- eventer/flask_server.py
+++ eventer/flask_server.py
@@ -23,4 +23,4 @@ def panadmin():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
