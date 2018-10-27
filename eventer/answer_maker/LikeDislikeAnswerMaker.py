368637f722dc45e1db8c5ceb6c6473250cecad35
«Dmitry <dimau777@gmail.com>
2018-10-27 16:21:00 +0300

    Add the first test for answering part of the service

--- eventer/answer_maker/LikeDislikeAnswerMaker.py
+++ eventer/answer_maker/LikeDislikeAnswerMaker.py
@@ -15,8 +15,13 @@ class LikeDislikeAnswerMaker(AbstractAnswerMaker):
 
         # We will work with last queue of events for this user and we will change it in database
         last_queue_events = self.user.last_queue_events
-        self.session.add(self.user)
         logging.debug('Value of last_queue_events: %s', last_queue_events)
+
+        # If we don't know which event user has liked (or disliked), because last_queue_events is empty
+        if len(last_queue_events) == 0:
+            answer["text"] = "Больше не осталось подходящих событий, поищем что-то еще?"
+            answer["status"] = "none_event"
+            return answer
 
         # Add like or dislike for previous event
         previous_event_id = last_queue_events[0]
@@ -29,18 +34,25 @@ class LikeDislikeAnswerMaker(AbstractAnswerMaker):
             answer["text"] = "Больше не осталось подходящих событий, поищем что-то еще?"
             answer["status"] = "none_event"
             self.user.clear_last_queue_events()
-        else:
-            next_event_id = last_queue_events[1]
-            next_event = self.session.query(Event).filter(Event._id == next_event_id).first()
-            if not next_event:
-                logging.error('Does not exist event with id: %s', next_event_id)
-                answer["text"] = "Больше не осталось подходящих событий, поищем что-то еще?"
-                answer["status"] = "none_event"
-                self.user.clear_last_queue_events()
-                return answer
-            answer.update(self.make_one_event_data_card(next_event))
-            self.user.delete_previous_event_from_queue()
+            self.session.add(self.user)
+            self.session.commit()
+            return answer
+
+        # If we have 2 events or more in last_queue_events
+        next_event_id = last_queue_events[1]
+        next_event = self.session.query(Event).filter(Event._id == next_event_id).first()
+        if not next_event:
+            logging.error('Does not exist event with id: %s', next_event_id)
+            answer["text"] = "Больше не осталось подходящих событий, поищем что-то еще?"
+            answer["status"] = "none_event"
+            self.user.clear_last_queue_events()
+            self.session.add(self.user)
+            self.session.commit()
+            return answer
+        answer.update(self.make_one_event_data_card(next_event))
+        self.user.delete_previous_event_from_queue()
 
         # Save all changes to database
+        self.session.add(self.user)
         self.session.commit()
         return answer
--- tests/controller/test_telegramController.py
+++ tests/controller/test_telegramController.py
@@ -1,12 +1,14 @@
 from AbstractTestClass import AbstractTestClass
 from TelegramController import TelegramController
 from AbstractView import AbstractView
+from Rating import Rating
+from User import User
 import datetime
 
 
 class TestTelegramController(AbstractTestClass):
 
-    def test_text_message_all_events_cinema_today(self, session, clear_data):
+    def test_telegram_text_message_find_events_cinema_tomorrow(self, session, clear_data):
         # Fulfilling the database for future making answer - film tomorrow
         start_time = datetime.datetime.now().timestamp() + 86200
         finish_time = datetime.datetime.now().timestamp() + 86300
@@ -26,6 +28,147 @@ class TestTelegramController(AbstractTestClass):
         assert test_bot.text == AbstractView.convert_timestamp_to_date_and_time(start_time) + \
                "<a href='https://afisha.yandex.ru/13Iaf1251.jpg' target='_blank'>.</a>\n" \
                "<a href='https://afisha.yandex.ru/moscow/cinema/venom-2018' target='_blank'>Веном</a>"
+
+    def test_telegram_text_message_fallback(self, session, clear_data):
+        # Creating objects that I will use in calling method under testing
+        controller = TelegramController()
+        controller.session = session
+        test_bot = TestBot()
+        test_update_from_telegram = TestUpdateFromTelegram(text="Белиберда",
+                                                           chat_id=234,
+                                                           effective_user_id=555,
+                                                           channel_post=None)
+        # Check result of calling "_text_message" method (this method pass result to the 'test_bot' object)
+        controller._text_message(test_bot, test_update_from_telegram)
+        assert test_bot.text == "Что-то я вас не понял, так какие события вас интересуют и когда?"
+
+    def test_telegram_text_message_show_favorites_for_new_user(self, session, clear_data):
+        # Creating objects that I will use in calling method under testing
+        controller = TelegramController()
+        controller.session = session
+        test_bot = TestBot()
+        test_update_from_telegram = TestUpdateFromTelegram(text="Избранное",
+                                                           chat_id=234,
+                                                           effective_user_id=555,
+                                                           channel_post=None)
+        # Check result of calling "_text_message" method (this method pass result to the 'test_bot' object)
+        controller._text_message(test_bot, test_update_from_telegram)
+        assert test_bot.text == "Здесь будет список мероприятий, которые тебе понравились"
+
+    def test_telegram_text_message_show_favorites_for_old_user(self, session, clear_data):
+        # Fulfilling the database for future making answer - film tomorrow
+        start_time = datetime.datetime.now().timestamp() + 86200
+        finish_time = datetime.datetime.now().timestamp() + 86300
+        event = self.create_event_yandexafishacinema_cinema_venom(start_time, finish_time)
+        session.add(event)
+        rating = Rating(user_id=1, event_id=1, like=1)
+        session.add(rating)
+        session.commit()
+        # Creating objects that I will use in calling method under testing
+        controller = TelegramController()
+        controller.session = session
+        test_bot = TestBot()
+        test_update_from_telegram = TestUpdateFromTelegram(text="Избранное",
+                                                           chat_id=234,
+                                                           effective_user_id=555,
+                                                           channel_post=None)
+        # Check result of calling "_text_message" method (this method pass result to the 'test_bot' object)
+        controller._text_message(test_bot, test_update_from_telegram)
+        assert test_bot.text == "Вот список мероприятий, которые тебе понравились\n\n" \
+            "⭐ <a href='https://afisha.yandex.ru/moscow/cinema/venom-2018' target='_blank'>Веном</a> " + \
+            AbstractView.convert_timestamp_to_date_and_time(start_time)
+
+    def test_telegram_text_message_greeting(self, session, clear_data):
+        # Creating objects that I will use in calling method under testing
+        controller = TelegramController()
+        controller.session = session
+        test_bot = TestBot()
+        test_update_from_telegram = TestUpdateFromTelegram(text="Привет",
+                                                           chat_id=234,
+                                                           effective_user_id=555,
+                                                           channel_post=None)
+        # Check result of calling "_text_message" method (this method pass result to the 'test_bot' object)
+        controller._text_message(test_bot, test_update_from_telegram)
+        assert test_bot.text == "Привет, дорогой друг! Какие мероприятия тебя интересуют?"
+
+    def test_telegram_text_message_help(self, session, clear_data):
+        # Creating objects that I will use in calling method under testing
+        controller = TelegramController()
+        controller.session = session
+        test_bot = TestBot()
+        test_update_from_telegram = TestUpdateFromTelegram(text="Помощь",
+                                                           chat_id=234,
+                                                           effective_user_id=555,
+                                                           channel_post=None)
+        # Check result of calling "_text_message" method (this method pass result to the 'test_bot' object)
+        controller._text_message(test_bot, test_update_from_telegram)
+        assert "Я подскажу, куда сходить в Москве" in test_bot.text
+
+    def test_telegram_text_message_like_or_dislike_for_long_queue_events(self, session, clear_data):
+        # Fulfilling the database for future making answer - adding two events
+        start_time = datetime.datetime.now().timestamp() + 86200
+        finish_time = datetime.datetime.now().timestamp() + 86300
+        event1 = self.create_event_yandexafishacinema_cinema_venom(start_time, finish_time)
+        session.add(event1)
+        event2 = self.create_event_kudago_exhibition_face2face()
+        session.add(event2)
+        # Adding information about last_queue_events like if this user have already tried to find events
+        user = User(telegram_id=777, name="Dima")
+        user.last_queue_events = [2, 1]
+        session.add(user)
+        session.commit()
+        # Creating objects that I will use in calling method under testing
+        controller = TelegramController()
+        controller.session = session
+        test_bot = TestBot()
+        test_update_from_telegram = TestUpdateFromTelegram(text="Нравится",
+                                                           chat_id=234,
+                                                           effective_user_id=777,
+                                                           channel_post=None)
+        # Check result of calling "_text_message" method (this method pass result to the 'test_bot' object)
+        controller._text_message(test_bot, test_update_from_telegram)
+        assert test_bot.text == AbstractView.convert_timestamp_to_date_and_time(start_time) + \
+               "<a href='https://afisha.yandex.ru/13Iaf1251.jpg' target='_blank'>.</a>\n" \
+               "<a href='https://afisha.yandex.ru/moscow/cinema/venom-2018' target='_blank'>Веном</a>"
+
+    def test_telegram_text_message_like_or_dislike_for_one_event_in_queue_events(self, session, clear_data):
+        # Fulfilling the database for future making answer - adding one event
+        event = self.create_event_yandexafishacinema_cinema_venom()
+        session.add(event)
+        # Adding information about last_queue_events like if this user have already tried to find events
+        user = User(telegram_id=777, name="Dima")
+        user.last_queue_events = [1]
+        session.add(user)
+        session.commit()
+        # Creating objects that I will use in calling method under testing
+        controller = TelegramController()
+        controller.session = session
+        test_bot = TestBot()
+        test_update_from_telegram = TestUpdateFromTelegram(text="Нравится",
+                                                           chat_id=234,
+                                                           effective_user_id=777,
+                                                           channel_post=None)
+        # Check result of calling "_text_message" method (this method pass result to the 'test_bot' object)
+        controller._text_message(test_bot, test_update_from_telegram)
+        assert test_bot.text == "Больше не осталось подходящих событий, поищем что-то еще?"
+
+    def test_telegram_text_message_like_or_dislike_for_zero_event_in_queue_events(self, session, clear_data):
+        # Adding information about last_queue_events with zero events
+        user = User(telegram_id=777, name="Dima")
+        user.last_queue_events = []
+        session.add(user)
+        session.commit()
+        # Creating objects that I will use in calling method under testing
+        controller = TelegramController()
+        controller.session = session
+        test_bot = TestBot()
+        test_update_from_telegram = TestUpdateFromTelegram(text="Нравится",
+                                                           chat_id=234,
+                                                           effective_user_id=777,
+                                                           channel_post=None)
+        # Check result of calling "_text_message" method (this method pass result to the 'test_bot' object)
+        controller._text_message(test_bot, test_update_from_telegram)
+        assert test_bot.text == "Больше не осталось подходящих событий, поищем что-то еще?"
 
 
 class TestUpdateFromTelegram:
